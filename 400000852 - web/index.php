528ed303d5ef02ed43e14e53faf7ae6beabe5d3e
TheRogueShon <47004552+TheRogueShon@users.noreply.github.com>
2020-11-27 22:48:19 -0400

    Add files via upload

---
+++ 400000852 - web/index.php
@@ -0,0 +1,10 @@
+<?php
+use Quwi\framework\FrontController;
+
+require 'd:\xampp\400000852\config.php';
+require ROOT_DIR . '\autoload.php';
+/*
+$controller = new IndexController();
+$controller->run(); */
+
+FrontController::run();
